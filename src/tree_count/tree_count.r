6fbcf1a36cd30bb017e93ed3df61adf58a35b25a
Nikolas Bertrand <code@nbertrand.dev>
2023-06-19 15:52:06 +0200

    tree-count_forestToolsTests.R playing with numbers, added filter! read_las

--- src/tree_count/tree_count.r
+++ src/tree_count/tree_count.r
@@ -2,9 +2,16 @@ library(ForestTools)
 library(raster)
 library(lidR)
 
-# create CHM 
-las <- readLAS(../../data/Golm_May06_2018_Milan_UTM33N_WGS84_6digit_cl.las)
+las <- readLAS("/home/nibe/PROJECTS/golm_tree_analysis/data/Golm_May06_2018_Milan_UTM33N_WGS84_6digit_cl.las")
 
 las <- segment_trees(las, li2012())
 col <- random.colors(200)
-plot(las, color = "treeID", colorPalette = col)
+
+plot(las, color = "treeID", pal = "auto")
+#CREATE CHM
+
+#USE LIDR tool to filter trees better: 
+
+#USE FORESTTOOLS TO COUNT TREES
+
+
