54562ac8e49b0f187ee57a1ad278713208149a0b
Nikolas Bertrand <code@nbertrand.dev>
2023-06-22 23:01:57 +0200

    570 trees ...

--- src/tree_count/tree_count.r
+++ src/tree_count/tree_count.r
@@ -3,12 +3,12 @@ library(ForestTools)
 library(raster)
 library(lidR)
 
-las <- readLAS("/home/nibe/PROJECTS/golm_tree_analysis/data/Golm_May06_2018_Milan_UTM33N_WGS84_6digit_cl.copc.laz")#, filter = "-drop_z_above 20")
+las <- readLAS("/home/nibe/PROJECTS/golm_tree_analysis/data/Golm_May06_2018_Milan_UTM33N_WGS84_6digit_cl.copc.laz", filter = "-keep_first")
 
 #las <- segment_trees(las, li2012())
-CHM <- rasterize_canopy(las, res=3.5, p2r(0.2), pkg = "raster")
+#CHM <- rasterize_canopy(las, res=4, p2r(0.2), pkg = "raster")
 
-
+CHM <- rasterize_canopy(las, res=5, p2r(subcircle = 0.15), pkg = "raster")
 
 
 #las <- segment_trees(las, li2012())
@@ -17,7 +17,7 @@ CHM <- rasterize_canopy(las, res=3.5, p2r(0.2), pkg = "raster")
 
 
 
-lin <- function(x){x * 0.03 + 0.5}
+lin <- function(x){x * 0.06 + 0.5}
 
 ttops <- vwf(CHM, winFun = lin, minHeight = 2)
 png("my_plot.png")
@@ -28,10 +28,10 @@ plot(ttops, col = "red", pch = 20, cex = 0.5, add = TRUE)
 #mean(ttops$height)
 
 # Create crown map
-crowns <- mcws(treetops = ttops, CHM , minHeight = 4, verbose = FALSE)
+#crowns <- mcws(treetops = ttops, CHM , minHeight = 4, verbose = FALSE)
 
 # Plot crowns
-plot(crowns, col = sample(rainbow(50), length(unique(crowns[])), replace = TRUE), legend = FALSE, xlab = "", ylab = "", xaxt='n', yaxt = 'n')
+#:plot(crowns, col = sample(rainbow(50), length(unique(crowns[])), replace = TRUE), legend = FALSE, xlab = "", ylab = "", xaxt='n', yaxt = 'n')
 dev.off()
 sp_summarise(ttops)
 
